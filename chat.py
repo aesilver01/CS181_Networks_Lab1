af05b0a3135c7ed7c73ee6fc1ae52677db3c7d3d
Alex Silver <asilver@hmc.edu>
2025-09-25 21:01:11 -0700

    still stalling out once it reaches waiting to connect

--- chat.py
+++ chat.py
@@ -7,100 +7,68 @@ Options for
 """""
 
 def help():
-    print("Options for printing:")
-    return
-
+	print("Options for printing:")
+	return None
 
 def myip():
-    localhost = socket.gethostname()
-    print("Host's IPv4 address is ", socket.gethostbyname(localhost))
+	localhost = socket.gethostname()
+	print("Host's IPv4 address is ", socket.gethostbyname(localhost))
+	return socket.gethostbyname(localhost)
 
-# port = 1234
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# def getuserinput():
+# 	# get user command
+# 	print("Running getuserinput")
+# 	userinput = input("Input command: ")
 
-# def setup_server(port_num):
-#     s = socket.socket()  # next create a socket object      
-#     s.bind(('', port_num))  # bind to the listening port
-#     print("bound to port", port_num)
+# 	if type(userinput) != str:
+# 		print("Invalid input, please enter a string")
+# 		return
+	
+# 	# check input 
+# 	if userinput == "help":
+# 		help()
 
-#     def capitalize_message(input_str):
-#         return input_str.upper()
+# 	if userinput == "myip":
+# 		myip()
 
-#     # # keep listening
-#     s.listen(1)  # maximum pending connections is 5 
-#     print("listening done")
-    
-#     connection_socket, address = s.accept()  # establish connection socket
-#     print("cs and address are", connection_socket, "and ", address)
-#     thread = threading.Thread(target=capitalize_message, 
-#                             args=(connection_socket, address),
-#                             daemon=True)
-#     print("made it to thread")
-#     connection_socket.send('Thank you for connecting'.encode()) 
-#     connection_socket.close()  # close connection
+# 	if userinput == "myport":
+# 		print("my ip is ", myip(), "of type", type(myip()))
+# 		print("and the socket is ", socket.getsockname())
+# 		print(myip().getsockname()[1])
 
-
-# def connect(dest, port):
-#     setup_server(num)
-#     print("a")
-#     s = socket.socket()
-#     print("b")
-#     # connect to the server on local computer 
-#     s.connect((dest, port)) 
-#     print("c")
-#     while True:
-#         message = input(">")
-#         s.send(message.encode())
-#         # receive data from the server and decoding to get the string.
-#         # print (s.recv(1024).decode())
-#         break
-#     # close the connection 
-#     s.close()
-
-
-#socket1 = setup_server(portnumber)
-#print("Listening socket's number is", socket1.getsockname()[1])
-
-userinput = ""
-def getuserinput():
-    # get user command
-    userinput = input("Input command: ")
-    
-    # check input 
-    if userinput == "help":
-        help()
-
-    if userinput == "myip":
-        myip()
-
-    if userinput == "myport":
-        print(myip().getsockname()[1])
-
-    if userinput == "connect":
-        ip = input("Enter IP address: ")
-        port = int(input("Enter port number: "))
-        connect(ip, port)
-        # connect("172.28.224.11")
+# 	if userinput == "connect":
+# 		ip = input("Enter IP address: ")
+# 		port = int(input("Enter port number: "))
+# 		if port < 1024 or port > 65535:
+# 			print("Invalid port number, please enter a number between 1024 and 65535")
+# 			return
+# 		elif port == myip().getsockname()[1]:
+# 			print("Cannot connect to self")
+# 			return
+# 		connect(ip, port)
+# 		# connect("172.28.224.11")
+# 	return
 
 def connect(ip, port):
-    print("Opened connection")
-    s = socket.socket()  # next create a socket object
-    s.connect((ip, port))  # connect to the server on local computer
-    # keep getting user input
-    while True:
-        message = input(">")
+	print("Opened connection")
+	s = socket.socket()  # next create a socket object
+	s.connect((ip, port))  # connect to the server on local computer
+	# keep getting user input
+	while True:
+		message = input(">")
 
-        # encode the message to bytes and send using the socket
-        s.send(message.encode())
+		# encode the message to bytes and send using the socket
+		s.send(message.encode())
 
-        # print replies from server
-        data = s.recv(1024)
-        print(data.decode())
-        
-        if message == "close":
-            break
+		# print replies from server
+		data = s.recv(1024)
+		print(data.decode())
+		
+		if message == "close":
+			print("Closing connection from client side")
+			break
 
-    s.close()
+	s.close()
 
 
 def handle_connection(connection_socket, address):
@@ -122,6 +90,7 @@ def handle_connection(connection_socket, address):
 		if message == "close":
 			break
 			
+	print("Closing connection from server side with", address)
 	connection_socket.close()
 
 
@@ -141,42 +110,63 @@ def handle_connection(connection_socket, address):
 #             break
 #         connection_socket.close()
 
+
+def input_handler(sock):
+	command = input("Input command: ")
+
+	if type(command) != str:
+		print("Invalid input, please enter a string")
+		return
+
+	if command == "help":
+		print("Options for printing:")
+
+	elif command == "myip":
+		hostname = socket.gethostname()
+		hostip = socket.gethostbyname(hostname)
+		print("The IPv4 address of this process is ", hostip)
+		return hostip
+	
+	elif command == "myport":
+		portnum = sock.getsockname()[1]
+		print("The listening socket of this process is ", portnum)
+		return(portnum)
+	
+	elif command == "connect":	
+		ip = input("Enter IP address: ")
+		port = int(input("Enter port number: "))
+		if port < 1024 or port > 65535:
+			print("Invalid port number, please enter a number between 1024 and 65535")
+			return
+		elif port == sock.getsockname()[1]:
+			print("Cannot connect to self")
+			return
+		connect(ip, port)
+	return
+
 def main():
-    # create a listening socket
-    port = int(sys.argv[1])
-    print("port is", port, type(port))
+	# create a listening socket
+	port = int(sys.argv[1])
+	s = socket.socket()
+	s.bind(("", port))  # input is a tuple with address and port as elements
+	print("Hello. Created new process with listening port", port)	
 
-    s = socket.socket()
-    s.bind(("", port))  # input is a tuple with address and port as elements
-    print("hello")
-    
+	s.listen(50)
 
-    s.listen(50)
-
-    i = 0
-    while i <3: 
-        getuserinput()
-        # accept connection request
-        print("do we hit this line")
-        connection_socket, address = s.accept()
-        print("Got connection from", address)
-        # create a thread to handle the accepted client
-        thread = threading.Thread(target=handle_connection, args=(connection_socket, address), daemon=True)
-        thread.start()  # start the thread
-
-
-
-# # keep listening
-# s.listen(5)  # maximum pending connections is 5 
-# while True: 
-#   connection_socket, address = s.accept()  # establish connection socket
-#   thread = threading.Thread(target=capitalize_message, 
-#                             args=(connection_socket, address),
-#                             daemon=True)
-#   connection_socket.send('Thank you for connecting'.encode()) 
-#   connection_socket.close()  # close connection
-#   break
+	i = 0
+	while i <3: 
+		# getuserinput()
+		print("looping", i)
+		input_handler(s)
+		# accept connection request
+		print("Waiting for connection...")
+		connection_socket, address = s.accept()
+		print("Got connection from", address)
+		# create a thread to handle the accepted client
+		thread = threading.Thread(target=handle_connection, args=(connection_socket, address), daemon=True)
+		thread.start()  # start the thread
+		print("started thread", thread.name)
 
 
 if __name__ == "__main__":
-    main()
+	main()
